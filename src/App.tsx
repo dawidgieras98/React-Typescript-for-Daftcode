6b00364d409f49fd555b04fc3c75a32ff0a8776d
dawidgieras98 <dawid.gieras98@gmail.com>
2022-05-22 11:38:56 +0100

    fix: logo has been removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const HandleClick = () => {
@@ -29,17 +28,6 @@ function App() {
             Live Code
           </a>
         </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
