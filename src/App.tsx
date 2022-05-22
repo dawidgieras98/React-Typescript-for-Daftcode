30ca74913d69e0b0faccf222eb973261240aa7b3
dawidgieras98 <97906443+dawidgieras98@users.noreply.github.com>
2022-05-22 10:41:40 +0100

    initial setup and cleanup before homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-const HandleClick = () => {
-  alert("Hello React!");
-};
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Pierwsze zabawy z Reactem!</p>
-        <button onClick={HandleClick}>Click Me</button>
-        <div className="link-box">
-          <a
-            className="App-link"
-            href="https://github.com/dawidgieras98/First-steps-with-react-typescript"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source Code - Github
-          </a>
-          <a
-            className="App-link"
-            href="https://fascinating-sherbet-65057a.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Live Code
-          </a>
-        </div>
-      </header>
-    </div>
-  );
+function Form() {
+  return <div className="App"></div>;
 }
 
-export default App;
+export default Form;
