189ce4721001aae2d2fc7b69a614702f26423ce5
dawidgieras98 <dawid.gieras98@gmail.com>
2022-05-22 23:55:37 +0100

    feature: interface of types and mechanism of input length has been added

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,54 @@ import React, { useState } from "react";
 import "./App.css";
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState("");
-  const [surName, setSurName] = useState("");
-  const [petName, setPetName] = useState("");
-  const [email, setEmail] = useState("");
+  interface initialValueConfig {
+    firstName: string;
+    lastName: string;
+    petName: string;
+    email: string;
+  }
+
+  const [initialValue, setInitialValue] = useState<initialValueConfig>({
+    firstName: "",
+    lastName: "",
+    petName: "",
+    email: "",
+  });
+  const [formError, setFormError] = useState<string>("");
+  const [errorAlert, setErrorAlert] = useState<boolean>(false);
+
+  const checkIfReadyToSubmit = !(errorAlert || !initialValue);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    // if(e=0){}
   };
+
+  function handleOnChangeInput(e: any) {
+    // if (initialValue.firstName.length < 3 || initialValue.firstName.length > 20) {
+    //   if (initialValue.firstName.length > 20) {
+    //     setFormError("A little bit too long");
+    //   }
+    //   if (initialValue.firstName.length < 3) {
+    //     setFormError("A little bit too Short");
+    //   }
+    //   setErrorAlert(true);
+    // } else {
+    //   setErrorAlert(false);
+    //   setFormError("");
+    // }
+  }
 
   return (
     <form className="Form" onSubmit={handleSubmit}>
+      {errorAlert && <div style={{ color: "red" }}>{formError}</div>}
       <fieldset>
         <label htmlFor="name">First name:</label>
         <input
           type="text"
           id="name"
           name="name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          value={initialValue.firstName}
+          onInput={handleOnChangeInput}
         />
       </fieldset>
       <fieldset>
@@ -30,8 +58,8 @@ const ControlledInputs = () => {
           type="text"
           id="surname"
           name="surname"
-          value={surName}
-          onChange={(e) => setSurName(e.target.value)}
+          value={initialValue.lastName}
+          onInput={handleOnChangeInput}
         />
       </fieldset>
       <fieldset>
@@ -40,8 +68,8 @@ const ControlledInputs = () => {
           type="text"
           id="petname"
           name="petname"
-          value={petName}
-          onChange={(e) => setPetName(e.target.value)}
+          value={initialValue.petName}
+          onInput={handleOnChangeInput}
         />
       </fieldset>
       <fieldset>
@@ -50,12 +78,14 @@ const ControlledInputs = () => {
           type="email"
           id="email"
           name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={initialValue.email}
+          onInput={handleOnChangeInput}
         />
       </fieldset>
       <div className="button-box">
-        <button type="submit">Add person to the tabel</button>
+        <button type="submit" disabled={!checkIfReadyToSubmit}>
+          Add person to the tabel
+        </button>
       </div>
     </form>
   );
