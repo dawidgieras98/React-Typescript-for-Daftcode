b83642c511d02c2ab10937f2d09358ff87b7d254
dawidgieras98 <dawid.gieras98@gmail.com>
2022-05-22 11:16:56 +0100

    fix: previous state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
 
 const HandleClick = () => {
   alert("Hello React!");
@@ -11,25 +9,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Pierwsze zabawy z Reactem!</p>
         <button onClick={HandleClick}>Click Me</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source Code - Github
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Code
-        </a>
+        <div className="link-box">
+          <a
+            className="App-link"
+            href="https://github.com/dawidgieras98/First-steps-with-react-typescript"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code - Github
+          </a>
+          <a
+            className="App-link"
+            href="https://fascinating-sherbet-65057a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live Code
+          </a>
+        </div>
       </header>
     </div>
   );
