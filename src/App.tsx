7e8a3e82cb4a79f24f3ace4ac4efdac55bf5572f
dawidgieras98 <dawid.gieras98@gmail.com>
2022-05-22 20:16:11 +0100

    feature: standard layout and structure of the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function Form() {
-  return <div className="App"></div>;
-}
+const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [surName, setSurName] = useState("");
+  const [petName, setPetName] = useState("");
+  const [email, setEmail] = useState("");
 
-export default Form;
+  const HandleSubmit = (e: any) => {
+    e.preventDefault();
+    // if(e=0){}
+  };
+
+  return (
+    <form className="Form" onSubmit={HandleSubmit}>
+      <fieldset>
+        <label htmlFor="name">First name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="surname">Name:</label>
+        <input
+          type="text"
+          id="surname"
+          name="surname"
+          value={surName}
+          onChange={(e) => setSurName(e.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="petname">Pet's name:</label>
+        <input
+          type="text"
+          id="petname"
+          name="petname"
+          value={petName}
+          onChange={(e) => setPetName(e.target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </fieldset>
+      <div className="button-box">
+        <button type="submit">Add person to the tabel</button>
+      </div>
+    </form>
+  );
+};
+
+export default ControlledInputs;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Form from "./App";
+import ControlledInputs from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Form />
+    <div className="wrapper">
+      <ControlledInputs />
+    </div>
   </React.StrictMode>
 );
 
